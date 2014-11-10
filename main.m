5a6a144580b55c8622854180dc1fc72e84f4cc30
Olmo Zavala <olmozvala@gmail.com>
2014-11-09 22:00:54 -0500

    Updated algorithm and code

--- main.m
+++ main.m
@@ -15,6 +15,81 @@ end
 
 %% Intrinsic camera parameters for Kinect camera
 cameraModel=[-525 0 320;0 -525 240;0 0 1]; 
+
+%% Using points given by proffesor for testing (12)
+x_l = [
+    434.00 360
+    468.00 542
+    275.00 393
+    297.00 542
+    400.00 484
+    424.00 625
+    242.00 483
+    261.00 628
+    631.00 541
+    655.00 622
+    787.00 549
+    650.00 443];
+
+x_r = [
+    428 352
+    447 545
+    255 389
+    265 545
+    407 481
+    421 629
+    239 481
+    248 631
+    625 544
+    671 627
+    797 544 
+    668 439];
+
+% Mean of points
+mux_l = mean(x_l);
+mux_r = mean(x_r);
+
+% Compute T_trans
+T_tl = [1 0 -mux_l(1); 0 1 -mux_l(2); 0 0 1];
+T_tr = [1 0 -mux_r(1); 0 1 -mux_r(2); 0 0 1];
+
+% Compute RMS
+RMS_l =  sqrt( (1/length(x_l))*sum( (x_l(:,1) - mux_l(1)).^2 + (x_l(:,2) - mux_l(2)).^2 ) );
+RMS_r =  sqrt( (1/length(x_r))*sum( (x_r(:,1) - mux_r(1)).^2 + (x_r(:,2) - mux_r(2)).^2 ) );
+
+% Compute T_scale
+T_sl = [sqrt(2)/RMS_l   0               0
+        0               sqrt(2)/RMS_l   0 
+        0               0               1];
+T_sr = [sqrt(2)/RMS_r   0               0
+        0               sqrt(2)/RMS_r   0 
+        0               0               1];
+
+% Tnorm
+T_nl = T_sl*T_tl
+T_nr = T_sr*T_tr
+
+% Adding extra column to make the multiplication
+x_l(:,3) = 1;
+x_r(:,3) = 1;
+
+% Normalizing points 
+uvl = T_nl*x_l'
+uvr = T_nr*x_r'
+
+% Build matrix A
+A = zeros(length(x_l),9); 
+for i=1:length(x_l)
+    A(i,:) = [uvl(1,i)*uvr(1,i) uvl(1,i)*uvr(2,i) uvl(1,i) uvl(2)*uvr(1) uvl(2,i)*uvr(2,i) uvl(2,i) uvr(1,i) uvr(2,i) 1];
+end
+
+% Compute SVD of A
+[U,S,VT] = svd(A);
+% Set f to be the last column of V
+f = VT(:,end);
+
+return;
+
 totalImages = 20;
 
 %% Read images and depth data (480 x 640 x channels(5) x totalImages(20))
